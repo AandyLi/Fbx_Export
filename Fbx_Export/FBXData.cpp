ee90d0a8b1722b2c2a285c4eddae54a4af4298ea
Jon4h <jonahnapieralla@gmail.com>
2017-04-12 09:52:17 +0200

    - Removed unnecessary code to improve readability. - Restructured FBXData to work with classes and structs instead of raw float data for storing meshes. - Started sorting through meshes and allocating memory for storing their data.

--- Fbx_Export/FBXData.cpp
+++ Fbx_Export/FBXData.cpp
@@ -1,12 +1,22 @@
 #include "FBXData.h"
 
-
-
-FBXData::FBXData()
-{
+FBXData::FBXData() {
 }
 
+FBXData::~FBXData() {
+	delete[] meshes;
+}
 
-FBXData::~FBXData()
-{
+void FBXData::AddMesh(int count) {
+	Mesh* newA = new Mesh[meshCount + 1];
+
+	for (int mesh = 0; mesh < meshCount; mesh++) {
+		newA[mesh] = meshes[mesh];
+	}
+	if (meshCount != 0) {
+		delete[] meshes;
+	}
+
+	meshes = newA;
+	meshCount++;
 }
--- Fbx_Export/FBXData.h
+++ Fbx_Export/FBXData.h
@@ -1,16 +1,38 @@
 #pragma once
-class FBXData
-{
-private:
 
+struct Vertex {
+	float position[3];
+	float uv[2];
+	float normal[3];
+};
+
+class Mesh {
 public:
+	Vertex* vertices;
+	int vertexCount = 0;
 
+	void addVertex(int count) {
+		Vertex* newA = new Vertex[vertexCount + 1];
 
-	float**** meshes;
+		for (int vertex = 0; vertex < vertexCount; vertex++) {
+			newA[vertex] = vertices[vertex];
+		}
+		if (vertexCount != 0) {
+			delete[] vertices;
+		}
 
+		vertices = newA;
+		vertexCount++;
+	}
+};
 
+class FBXData {
+public:
+	Mesh* meshes;
+	int meshCount = 0;
 
 	FBXData();
 	~FBXData();
-};
 
+	void AddMesh(int);
+};
